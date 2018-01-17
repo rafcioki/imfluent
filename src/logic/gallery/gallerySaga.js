ee7deb365856c1e0f51a27fd55a5f06f74762c62
Rafal Ostrowski <oester200729@gmail.com>
2018-01-17 23:58:30 +0100

    Missing saga.

---
+++ src/logic/gallery/gallerySaga.js
@@ -0,0 +1,39 @@
+import { put, takeLatest, select } from 'redux-saga/effects'
+import { findIndex } from 'underscore'
+import { movePinnedImageLeft, movePinnedImageRight, pinImage } from './galleryActions'
+
+function* getIndexOfCurrentlyPinnedImage() {
+  const state = yield select()
+  const images = state.images.images
+  const currentlyPinnedImageUrl = state.gallery.pinnedImageUrl
+
+  return findIndex(images, img => img.imageUrl === currentlyPinnedImageUrl)
+}
+
+function* moveImageLeft(action) {
+  const state = yield select()
+  const index = yield getIndexOfCurrentlyPinnedImage()
+  const images = state.images.images
+
+  if (index > 0) {
+    yield put(pinImage(images[index - 1].imageUrl))
+  }
+}
+
+function* moveImageRight(action) {
+  const state = yield select()
+  const index = yield getIndexOfCurrentlyPinnedImage()
+  const images = state.images.images
+
+  if (index < images.length - 1) {
+    yield put(pinImage(images[index + 1].imageUrl))
+  }
+}
+
+export function* watchMovePinnedImageLeft() {
+  yield takeLatest('MOVE_PINNED_IMAGE_LEFT', moveImageLeft)
+}
+
+export function* watchMovePinnedImageRight() {
+  yield takeLatest('MOVE_PINNED_IMAGE_RIGHT', moveImageRight)
+}
